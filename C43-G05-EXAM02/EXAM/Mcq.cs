d3af355925e8073623eb8efcc300c9b0edd986c6
Greg Low <greg@gmail.com>
2025-01-21 22:20:26 +0200

    Exam

---
+++ C43-G05-EXAM02/EXAM/Mcq.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C43_G05_EXAM02.EXAM
+{
+    public class MCQQuestion : Question
+    {
+        public MCQQuestion(string header, string body, int mark)
+            : base(header, body, mark)
+        {
+        }
+
+        public override void DisplayQuestion()
+        {
+            Console.WriteLine($"{Header}\n{Body}");
+            foreach (var answer in Answers)
+            {
+                Console.WriteLine(answer);
+            }
+        }
+
+        public override bool CheckAnswer(int answerId)
+        {
+            return CorrectAnswer.AnswerId == answerId;
+        }
+    }
+}
---
+++ C43-G05-EXAM02/EXAM/Trueorfalse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C43_G05_EXAM02.EXAM
+{
+    public class TrueFalseQuestion : Question
+    {
+        public TrueFalseQuestion(string header, string body, int mark)
+            : base(header, body, mark)
+        {
+            Answers.Add(new Answer(1, "True"));
+            Answers.Add(new Answer(2, "False"));
+        }
+
+        public override void DisplayQuestion()
+        {
+            Console.WriteLine($"{Header}\n{Body}");
+            foreach (var answer in Answers)
+            {
+                Console.WriteLine(answer);
+            }
+        }
+
+        public override bool CheckAnswer(int answerId)
+        {
+            return CorrectAnswer.AnswerId == answerId;
+        }
+    }
+}
